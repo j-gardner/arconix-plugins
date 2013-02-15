5747473c9f2f3fa358634b8caf7f578765a73a41
John Gardner <john@arconixpc.com>
2013-02-15 11:05:59 -0500

    Update defaults to try to fix foreach error

--- includes/views/defaults.php
+++ includes/views/defaults.php
@@ -19,63 +19,63 @@ $defaults = array(
                 'not_found'             => __( 'No plugins found',              'acpl' ),
                 'not_found_in_trash'    => __( 'No plugins found in the trash', 'acpl' )
             ),
-            'public'                => true,
-            'query_var'             => true,
-            'menu_position'         => 100,
-            'has_archive'           => true,
-            'supports'              => array( 'title', 'thumbnail', 'excerpt' ),
-            'rewrite'               => array( 'slug' => 'plugins', 'with_front' => false )
+            'public'            => true,
+            'query_var'         => true,
+            'menu_position'     => 100,
+            'has_archive'       => true,
+            'supports'          => array( 'title', 'thumbnail', 'excerpt' ),
+            'rewrite'           => array( 'slug' => 'plugins', 'with_front' => false )
         )
 	),
 	'meta_box' => array(
-        'id' => 'plugins_box',
-        'title' => 'Plugin Details',
-        'pages' => array( 'plugins' ), // post type
-        'context' => 'normal',
-        'priority' => 'high',
-        'show_names' => true, // Show field names left of input
-        'fields' => array(
+        'id'            => 'plugins_box',
+        'title'         => 'Plugin Details',
+        'pages'         => array( 'plugins' ), // post type
+        'context'       => 'normal',
+        'priority'      => 'high',
+        'show_names'    => true, // Show field names left of input
+        'fields'        => array(
             array(
-                'name'      => 'Slug',
-                'desc'      => 'Enter the plugin\'s slug',
-                'id'        => $prefix . 'slug',
-                'type'      => 'text_medium',
+                'name'  => 'Slug',
+                'desc'  => 'Enter the plugin slug',
+                'id'    => $prefix . 'slug',
+                'type'  => 'text_medium',
             ),
             array(
-                'name'      => 'Demo',
-                'desc'      => 'Enter the demo URL',
-                'id'        => $prefix . 'demo',
-                'type'      => 'text_medium',
+                'name'  => 'Demo',
+                'desc'  => 'Enter the demo URL',
+                'id'    => $prefix . 'demo',
+                'type'  => 'text_medium',
             ),
             array(
-                'name'      => 'Download',
-                'desc'      => 'Enter the download URL.',
-                'id'        => $prefix . 'download',
-                'type'      => 'text_medium'
+                'name'  => 'Download',
+                'desc'  => 'Enter the download URL',
+                'id'    => $prefix . 'download',
+                'type'  => 'text_medium',
             ),
             array(
-                'name'      => 'Documentation',
-                'desc'      => 'Enter the documentation URL.',
-                'id'        => $prefix . 'docs',
-                'type'      => 'text_medium'
+                'name'  => 'Documentation',
+                'desc'  => 'Enter the documentation URL',
+                'id'    => $prefix . 'docs',
+                'type'  => 'text_medium',
             ),
             array(
-                'name'      => 'Support',
-                'desc'      => 'Enter the support URL.',
-                'id'        => $prefix . 'help',
-                'type'      => 'text_medium'
+                'name'  => 'Support',
+                'desc'  => 'Enter the support URL.',
+                'id'    => $prefix . 'help',
+                'type'  => 'text_medium',
             ),
             array(
-                'name'      => 'Development',
-                'desc'      => 'Enter the development board URL.',
-                'id'        => $prefix . 'dev',
-                'type'      => 'text_medium'
+                'name'  => 'Development',
+                'desc'  => 'Enter the development board URL',
+                'id'    => $prefix . 'dev',
+                'type'  => 'text_medium',
             ),
             array(
-                'name'      => 'Source Code',
-                'desc'      => 'Enter the source code URL.',
-                'id'        => $prefix . 'source',
-                'type'      => 'text_medium'
+                'name'  => 'Source Code',
+                'desc'  => 'Enter the source code URL',
+                'id'    => $prefix . 'source',
+                'type'  => 'text_medium',
             )
         )
     )
