56e14f5443549ba173e8feb50db7a5bf8fa5c2ba
John Gardner <john@arconixpc.com>
2014-11-29 10:56:16 -0500

    add $inc var

--- plugin.php
+++ plugin.php
@@ -19,11 +19,19 @@ class Arconix_Plugins {
     /**
      * Stores the current version of the plugin.
      *
-     * @since   0.1
-     * @version 1.0.0
-     * @var     string  $version    current plugin version
+     * @since   0.5
+     * @var     string  $version    Current plugin version
      */
-    public static $version = '1.0.0';
+    private $version = '1.0.0';
+
+    /**
+     * The directory path to this plugin's 'includes' folder.
+     *
+     * @since   1.0.0
+     * @access  private
+     * @var     string      $inc    The directory path to this plugin's 'includes' folder
+     */
+    private $inc;
 
     /**
      * Initialize the class and set its properties.
@@ -31,6 +39,7 @@ class Arconix_Plugins {
      * @since   1.0.0
      */
     public function __construct() {
+        $this->inc = trailingslashit( plugin_dir_path( __FILE__ ) . '/includes' );
         $this->load_dependencies();
         $this->load_admin();
     }
@@ -43,18 +52,34 @@ class Arconix_Plugins {
      * - Widgets registers the site widgets
      */
     private function load_dependencies() {
-        require_once( plugin_dir_path( __FILE__ ) . '/includes/class-arconix-plugins-admin.php' );
-        require_once( plugin_dir_path( __FILE__ ) . '/includes/class-arconix-plugin.php' );
+        require_once( $this->inc . 'class-arconix-plugins-admin.php' );
+        require_once( $this->inc . 'class-arconix-plugins-public.php' );
+        require_once( $this->inc . 'class-arconix-plugins-widgets.php' );
+
+        if ( ! class_exists( 'cmb_Meta_Box' ) )
+            require_once( $this->inc . 'metabox/init.php');
+
+        if ( ! class_exists( 'Gamajo_Dashboard_Glancer' ) )
+            require_once( $this->inc . 'class-gamajo-dashboard-glancer.php' );
     }
 
-
+    /**
+     * Load the Administrative Backend
+     *
+     * @since   1.0.0
+     */
     private function load_admin() {
         new Arconix_Plugins_Admin( $this->get_version() );
     }
 
-
+    /**
+     * Get the current version of the plugi9n
+     *
+     * @since   1.0.0
+     * @return  string  current plugin version
+     */
     public function get_version() {
-        return self::version;
+        return $this->version;
     }
 }
 
